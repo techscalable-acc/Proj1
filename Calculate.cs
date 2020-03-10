4bb289128f10175a2f45f65685d7a03a37f74309
Mamta Jha <mamta.jha@techscalable.com>
2020-03-10 09:45:34 +0530

    added new class calculate

---
+++ Calculate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace project1
+{
+    public class Calculate
+    {
+        public Calculate()
+        {
+        }
+        public int addition( int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+    }
+}
