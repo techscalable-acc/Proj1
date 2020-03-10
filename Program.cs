b16f339e62c5d9b9dc6e959debda9d014aceda69
techscalable-acc <60865223+techscalable-acc@users.noreply.github.com>
2020-03-10 10:44:59 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace project1
             Console.WriteLine("Hello World!");
             Console.WriteLine("First Change!");
             Console.WriteLine("Second Change!");
-            Console.WriteLine("Added at remote!");
+            Console.WriteLine("Added at git hub web console!");
 
             var calculate = new Calculate();
             int sum = calculate.addition(4, 5);
